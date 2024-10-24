7b44482fc3d459556d5d421418e3c3fb334301ff
Godwin peter .O <me@godwin.dev>
2024-10-24 20:21:57 +0100

    aaded new support pages

---
+++ src/login/FaqPage.jsx
@@ -0,0 +1,12 @@
+import LoginLayout from './LoginLayout';
+
+const FaqPage = () => {
+
+    return (
+        <LoginLayout>
+            <div>{'support.faqs'}</div>
+        </LoginLayout>
+    )
+}
+
+export default FaqPage;
