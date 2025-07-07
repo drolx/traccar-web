d79c8fdf8bfa67cb5ad2f1a23ce54faa73ba93d0
Godwin peter. O <me@godwin.dev>
2025-07-07 13:39:37 +0100

    merge commit for v6.8 - 8

--- src/main/MainToolbar.jsx
+++ src/main/MainToolbar.jsx
@@ -4,9 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import {
   Toolbar, IconButton, OutlinedInput, InputAdornment, Popover, FormControl, InputLabel, Select, MenuItem, FormGroup, FormControlLabel, Checkbox, Badge, ListItemButton, ListItemText, Tooltip,
 } from '@mui/material';
-import { makeStyles } from 'tss-react/mui';
-import { useTheme } from '@mui/material/styles';
-import MapIcon from '@mui/icons-material/Map';
+import { makeStyles, useTheme } from '@mui/styles';
+import MapIcon from '@mui/icons-material/MenuOpen';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import AddIcon from '@mui/icons-material/Add';
 import TuneIcon from '@mui/icons-material/Tune';
