c7d61a0d505cbc7923cdb1b3ca300816c453b604
Godwin peter. O <me@godwin.dev>
2025-07-07 12:35:48 +0100

    merge commit for v6.8 - 3
    Conflicts:
    	src/common/components/DesktopMenu.jsx
    	src/main/DeviceRow.jsx
    
    Changes to be committed:
    	modified:   src/main/DeviceRow.jsx

--- src/main/DeviceRow.jsx
+++ src/main/DeviceRow.jsx
@@ -105,13 +105,13 @@ const DeviceRow = ({ data, index, style }) => {
   const primarySection = () => {
     const time = dayjs(item.lastUpdate).fromNow() ?? 'None';
 
-    return (<div>
-      <div className={classes.mnimalText}>{time}</div>
-      <div>
+    return (<>
+      <span className={classes.mnimalText}>{time}</span>
+      <span>
         {item[devicePrimary]}
         {deviceSecondary && item[deviceSecondary] && ` • ${item[deviceSecondary]}`}
-      </div>
-    </div>)
+      </span>
+    </>)
   };
 
   const secondarySection = () => {
@@ -147,7 +147,7 @@ const DeviceRow = ({ data, index, style }) => {
           }}
         />
         {position && (
-          <div>
+          <div style={{ whiteSpace: 'nowrap' }}>
             <span className={classes.mnimalText}>{formatSpeed(position.speed, speedUnit, t)}</span>
             {position.attributes.hasOwnProperty('alarm') && (
               <Tooltip title={`${t('eventAlarm')}: ${formatAlarm(position.attributes.alarm, t)}`}>
